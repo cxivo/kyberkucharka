5ba51d7496141b7acf18a2fd19ed5ed70c4932ca
cxivo <marek14michalovic@gmail.com>
2024-04-14 17:37:13 +0200

    fixed CORS (so... CORS correction)

--- kyberkucharka-server/app.js
+++ kyberkucharka-server/app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
+const cors = require("cors"); // of CORS it didn't work
+app.use(cors());
+
 require("dotenv").config();
 
-const pgp = require("pg-promise")();
-const db = pgp(process.env.DATABAZA);
-
 app.get("/", async (req, res) => {
+  const pgp = require("pg-promise")();
+  const db = pgp(process.env.DATABAZA);
+
   const users = await db.any("SELECT * FROM users;");
 
   return res.json(users);
--- kyberkucharka-ui/src/recipe/communication.js
+++ kyberkucharka-ui/src/recipe/communication.js
@@ -1,5 +1,5 @@
 export async function getRecipes() {
-  return fetch("https://kyberkucharka-server.vercel.app/api/hello")
+  return fetch("https://kyberkucharka.onrender.com/")
     .then((response) => {
       if (!response.ok) {
         throw new Error("Error getting recipe list");
