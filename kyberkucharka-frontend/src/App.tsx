f9fc51f77593c7111079558d58221ff3681e4b14
cxivo <marek14michalovic@gmail.com>
2025-03-16 18:51:52 +0100

    modify styles

--- kyberkucharka-frontend/src/App.tsx
+++ kyberkucharka-frontend/src/App.tsx
@@ -1,57 +1,27 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import Recipe from "./Recipe";
 import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <>
-      <Link to="/">Kyberkuchárka</Link>
-      <div>prihlásiť sa</div>
-    </>
+    <nav>
+      <Link className="mainLogo" to="/">
+        Kyberkuchárka
+      </Link>
+      <div id="userDiv">
+        <div>prihlásiť sa</div>
+        <div>registrovať sa</div>
+      </div>
+    </nav>
   );
 }
 
-function App({ children }) {
+function App({ children }: any) {
   return (
     <>
       <Header></Header>
       <h1>Vitajte na hlavnej stránke!</h1>
       <Link to="/about">O projekte</Link>
       <div>{children}</div>
-    </>
-  );
-}
-
-
-
-function OldApp() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-        	Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
--- kyberkucharka-frontend/src/Recipe.tsx
+++ kyberkucharka-frontend/src/Recipe.tsx
@@ -1,11 +1,11 @@
 export default function Recipe() {
   return (
-    <>
+    <div className="recipe">
       <h2>Názov receptu</h2>
       <img src="https://i.imgur.com/MK3eW3Am.jpg" alt="Katherine Johnson" />
       <p>Tento recept robia odjakživa moja stará mama (na obrázku)</p>
       <h3>Ingrediencie</h3>
-      <ul>
+      <ul className="ingredientList">
         <li>Milý pohľad</li>
         <li>Vrúcne srdce</li>
         <li>Veľa lásky</li>
@@ -13,6 +13,6 @@ export default function Recipe() {
       </ul>
       <h3>Postup</h3>
       <p>Všetko zmiešame a posypané láskou podávame.</p>
-    </>
+    </div>
   );
 }
