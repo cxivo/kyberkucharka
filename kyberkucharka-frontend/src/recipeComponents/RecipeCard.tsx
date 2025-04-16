efb7917e2906e93674fb0e0d6d692e9fffe92c53
cxivo <marek14michalovic@gmail.com>
2025-04-16 17:40:36 +0200

    modify recipe cards style

--- kyberkucharka-frontend/src/recipeComponents/RecipeCard.tsx
+++ kyberkucharka-frontend/src/recipeComponents/RecipeCard.tsx
@@ -7,13 +7,15 @@ interface RecipeCardProps {
 
 export default function RecipeCard({recipe}: RecipeCardProps) {
     return (
-        
-        <Link to={`/recipes/${recipe.id}`} className="card">
-            
-                <h3>{recipe.title}</h3>
-                <em>{recipe.author.display_name}</em>
-                <p>{recipe.description}</p>
-        </Link>           
-
+      <Link to={`/recipes/${recipe.id}`} className="card">
+        <h3>{recipe.title}</h3>
+        {recipe.image_link && (
+          <img src={recipe.image_link} alt="obrázok k receput"></img>
+        )}
+        <p>
+          autor: <em>{recipe.author.display_name}</em>
+        </p>
+        <p>{recipe.description}</p>
+      </Link>
     );
 }
