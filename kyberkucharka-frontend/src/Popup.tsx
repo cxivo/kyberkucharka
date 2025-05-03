d52c58b41e0240efd57d36f7ab8da2b036c0fc5f
cxivo <marek14michalovic@gmail.com>
2025-05-03 14:48:22 +0200

    fix recipe covering edit buttons on phone screens

--- kyberkucharka-frontend/src/Popup.tsx
+++ kyberkucharka-frontend/src/Popup.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function Popup() {
   const [
